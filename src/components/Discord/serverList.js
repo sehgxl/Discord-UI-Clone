c3b3b4bc3b22871eb71b6818d05c7a329788d53d
sehgalabhinav <83122406+sehgalabhinav@users.noreply.github.com>
2022-07-18 16:22:15 +0530

    fixing server switching

--- src/components/Discord/serverList.js
+++ src/components/Discord/serverList.js
@@ -9,12 +9,15 @@ const ServerList = () => {
   const Navigate = useNavigate();
   return (
     <section className="bg-[#202225] flex flex-col items-center py-6 overflow-hidden">
-      <DiscordLogo className="mb-3 text-white rounded-2xl p-2 w-14 h-14 bg-[#5865F2] " />
+      <DiscordLogo
+        onClick={() => Navigate("/DiscordDashboard")}
+        className="mb-3 text-white rounded-2xl p-2 w-14 h-14 bg-[#5865F2] cursor-pointer "
+      />
       <img
         onClick={() => Navigate("/DiscordServer")}
         src={NewLineLogo}
         alt=""
-        className="mb-3 w-14 h-14 rounded-2xl"
+        className="mb-3 w-14 h-14 rounded-2xl cursor-pointer"
       />
       <PlusLogo className="mb-3 w-14 h-14 p-4 bg-[#36393F] rounded-full hover:bg-neutral-600" />
       <DownloadLogo className="mb-3 fill-[#43B581] w-14 h-14 p-4 bg-[#36393F] rounded-full hover:bg-neutral-600" />
